e11141acf12100963d8888e3b2c90ff51a6d5210
rgrunberg <33644220+rgrunberg@users.noreply.github.com>
2020-07-12 13:36:41 -0700

    Updates

---
+++ precache-manifest.7bf34d749e207d09fbb241b0f7f7925d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d74caebb4e156abcd78ab14ecaa514",
+    "url": "/wordgrid/index.html"
+  },
+  {
+    "revision": "0530be2264576199a8ad",
+    "url": "/wordgrid/static/css/main.615bb1d3.chunk.css"
+  },
+  {
+    "revision": "983e44c63e85348b1000",
+    "url": "/wordgrid/static/js/2.4f128868.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wordgrid/static/js/2.4f128868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0530be2264576199a8ad",
+    "url": "/wordgrid/static/js/main.3fceb6a2.chunk.js"
+  },
+  {
+    "revision": "8d8ebabca6518ea08c61",
+    "url": "/wordgrid/static/js/runtime-main.50d61006.js"
+  }
+]);
