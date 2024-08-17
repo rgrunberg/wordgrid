59b312d9e1b1f0faf44321b2047766a5e15957f6
rgrunberg <33644220+rgrunberg@users.noreply.github.com>
2024-08-17 09:36:06 -0700

    add build folder

---
+++ build/precache-manifest.7ca7d01a68da5e23b72e241acb0ce756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e4d62d36393030259a2ac76bae1224",
+    "url": "/wordgrid/index.html"
+  },
+  {
+    "revision": "895f360177e63abe7ffd",
+    "url": "/wordgrid/static/css/main.517ee738.chunk.css"
+  },
+  {
+    "revision": "983e44c63e85348b1000",
+    "url": "/wordgrid/static/js/2.4f128868.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wordgrid/static/js/2.4f128868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895f360177e63abe7ffd",
+    "url": "/wordgrid/static/js/main.c33ce4f9.chunk.js"
+  },
+  {
+    "revision": "8d8ebabca6518ea08c61",
+    "url": "/wordgrid/static/js/runtime-main.50d61006.js"
+  }
+]);
